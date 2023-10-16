4500700af3c73a2dd6b61f2ed512c43ee470a608
samivali <samantha.vali36@gmail.com>
2023-10-15 20:46:33 -0700

    week 1 progress, web bones, js web and api interaction started

---
+++ getData.js
@@ -0,0 +1,26 @@
+function getWeather() {
+    const apiKey = '870a178f80fb906f24cef2781fdcca82';
+    let c = document.getElementById("city");
+  
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${c}&appid=${apiKey}&units=metric`;
+  
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Update the UI with the weather data
+        document.getElementById(c).textContent = data.name;
+        document.getElementById('temperature').textContent = data.main.temp;
+        // Add more code to display other weather data as needed
+      })
+      .catch((error) => {
+        console.error('Fetch error:', error);
+        // Handle errors (e.g., display an error message to the user)
+      });
+  }
+
+  

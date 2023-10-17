d944dabb371a6546bccac69eefdc6b6504a928d3
samivali <samantha.vali36@gmail.com>
2023-10-16 21:32:36 -0700

    temp display

--- getData.js
+++ getData.js
@@ -1,8 +1,8 @@
 function getWeather() {
-    const apiKey = '';
-    let c = document.getElementById("city");
+    const apiKey = '870a178f80fb906f24cef2781fdcca82';
+    const city = document.getElementById('city').value;
   
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${c}&appid=${apiKey}&units=metric`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   
     fetch(apiUrl)
       .then((response) => {
@@ -12,15 +12,19 @@ function getWeather() {
         return response.json();
       })
       .then((data) => {
-        // Update the UI with the weather data
-        document.getElementById(c).textContent = data.name;
+        document.getElementById('city').textContent = data.name;
+          console.log('Data received from API:', data);
+          console.log('Updating temperature element:', data.main.temp);
+
         document.getElementById('temperature').textContent = data.main.temp;
         // Add more code to display other weather data as needed
       })
       .catch((error) => {
         console.error('Fetch error:', error);
-        // Handle errors (e.g., display an error message to the user)
       });
+
+      
+
   }
 
   

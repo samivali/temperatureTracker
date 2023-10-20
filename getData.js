3fe96503820dd74d49a1c07d206d47ae0ae9d8bc
samivali <samantha.vali36@gmail.com>
2023-10-20 15:03:26 -0700

    css updates

--- getData.js
+++ getData.js
@@ -1,17 +1,13 @@
 function getWeather() {
-    const apiKey = '870a178f80fb906f24cef2781fdcca82';
-    const city = document.getElementById('city').value;
-    const lat = 0;
-    const lon=0;
-    const dt=0;
-
-  
+    let apiKey = '';
+    let city = document.getElementById('city').value;
+    let lat = 0;
+    let lon=0;
+    let dt=0;
     const curApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
-    //find more user friendly way of getting lattitude and longitude ==> computer conversion??
-    //hard code different timestamps
-    const hisApiUrl = 'https://history.openweathermap.org/data/3.0/history/timemachine?lat='+{lat}+'&lon='+{lon}+'&dt='+{dt}+'&appid='+{apiKey};
-
+    const hisApiUrl = 'https://history.openweathermap.org/data/3.0/history/timemachine?lat='+lat+'&lon='+lon+'&dt='+dt+'&appid='+apiKey;
+//current data
     fetch(curApiUrl)
       .then((response) => {
         if (!response.ok) {
@@ -19,17 +15,19 @@ function getWeather() {
         }
         return response.json();
       })
-
-    
-
       .then((data) => {
         document.getElementById('city').textContent = data.name;
         document.getElementById('temperature').textContent = data.main.temp;
+
+        //sets lat + lon
+        lat=data.coord.lat;
+        lon=data.coord.lon;
           console.log('Data received from API:', data);
           console.log('Updating temperature element:', data.main.temp);
 
       })
-
+//5 years
+      dt=	1546234972;
     fetch(hisApiUrl)
       .then((response) =>{
         if(!response.okay){
@@ -38,7 +36,6 @@ function getWeather() {
         return response.json();
       })
       .then((data) =>{
-//need to break up years bc each api call only takes in 1 time stamp
         document.getElementById('temp-5-yrs').textContent = data.main.temp;
         //document.getElementById('temp-10-Yrs').textContent = data.main.temp;
         //document.getElementById('temp-20-Yrs').textContent = data.main.temp;
@@ -46,11 +43,50 @@ function getWeather() {
         console.log('Updating temperature element:', data.main.temp);
 
       })
+
+
+//10 years
+      dt =	1388468572;
+    fetch(hisApiUrl)
+      .then((response)=>{
+        if(!response.okay){
+          throw new Error('Network response was not okay');
+        }
+        return response.json();
+      })
     
       .catch((error) => {
         console.error('Fetch error:', error);
-      });
+      })
+      .then((data) =>{
+        document.getElementById('temp-10-Yrs').textContent = data.main.temp;
+        console.log('Data received from API:', data);
+        console.log('Updating temperature element:', data.main.temp);
 
+      })
+
+    
+ //20 years
+    dt =	757316572;
+    fetch(hisApiUrl)
+      .then((response)=>{
+        if(!response.okay){
+          throw new Error('Network response was not okay');
+        }
+        return response.json();
+      })
+    
+      .catch((error) => {
+        console.error('Fetch error:', error);
+      })
+      .then((data) =>{
+        document.getElementById('temp-20-Yrs').textContent = data.main.temp;
+        console.log('Data received from API:', data);
+        console.log('Updating temperature element:', data.main.temp);
+
+      })
+
+  
   }
 
 
